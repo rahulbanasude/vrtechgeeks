782f8a2e260fe499d6a1f88b1b0d899d6bf81d84
rahulbanasude <rahul.bansude2@gmail.com>
2017-06-02 19:06:47 +0530

    added about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
 resources :posts, only: [:index, :show]
   root 'posts#index'
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
